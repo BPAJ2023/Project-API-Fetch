584303c0b7451cfc88768023906198d00853c78f
Bartek Pośpiech <patka11nero@gmail.com>
2023-05-08 16:46:35 +0200

    edit main.js

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+const form = document.querySelector('form');
+const numUser = form.elements.numUser;
+const gender = form.elements.gender;
+
+
+const url = `https://randomuser.me/api/?results=${numUser}&gender=(${gender} === 'both ? "female,male" : ${gender}')`;
 fetch(url)
-.then
-.catch
+    .then(res=>res)
+    .then(res=>{
+        res.json;
+        console.log(res)
+    })
+    .catch(err=>err)
+
+form.addEventListener('submit', ()=>{
+    console.log(numUser,gender)
+});
