c41c94ac5e665279841647b312a24ae03f790c8d
Bartek Pośpiech <patka11nero@gmail.com>
2023-05-09 15:59:44 +0200

    "edit file index.html, add style content and create code of main.js"

--- main.js
+++ main.js
@@ -1,17 +1,40 @@
-const form = document.querySelector('form');
-const numUser = form.elements.numUser;
-const gender = form.elements.gender;
+const getResult = (e) => {
+    e.preventDefault();
+    const form = document.querySelector('form');
+    const genderInput = form.elements.gender;
+    const genderData = genderInput.selectedOptions[0].getAttribute('name');
+    const numUser = form.elements.numUser.value;
+    const genderParam = genderData === 'both' ? 'female, male' : genderData;
+    const url = `https://randomuser.me/api/?results=${numUser}&gender=${genderParam}`;
+    
+    
+    fetch(url)
+        .then(res => res.json())
+        .then(data => addUserToPage(data.results))
+        .catch(err => alert(err));
+
+};
 
 
-const url = `https://randomuser.me/api/?results=${numUser}&gender=(${gender} === 'both ? "female,male" : ${gender}')`;
-fetch(url)
-    .then(res=>res)
-    .then(res=>{
-        res.json;
-        console.log(res)
-    })
-    .catch(err=>err)
+const addUserToPage = (data) => {
+    const content = document.querySelector('.content');
+    content.textContent = '';
 
-form.addEventListener('submit', ()=>{
-    console.log(numUser,gender)
-});
+    console.log(data);
+
+    data.forEach(item=>{
+        const userItem = document.createElement('div');    
+        userItem.className = "content__item";    
+        userItem.innerHTML = `    
+        <img class="content__image" src= '${item.picture.medium}'>    
+        <div class="content__txt">
+        <span>${item.name.title}</span>
+        <span>${item.name.first}</span>
+        <span>${item.name.last}</span>
+        </div>    
+        `;
+        content.appendChild(userItem);
+})
+};
+
+document.querySelector('form').addEventListener('submit', getResult);
